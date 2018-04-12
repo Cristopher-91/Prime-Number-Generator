fb3da860b522bfda69ede63b753740aadd785651
Cristopher-91 <k.piasecki91@gmail.com>
2018-04-12 10:50:03 +0200

    Logic created

---
+++ src/sample/controller/Controller.java
@@ -0,0 +1,30 @@
+package sample.controller;
+
+import java.util.*;
+
+public class Controller {
+
+    public static List<Integer> GeneratePrime(int n) {
+        List<Integer> primeNumbers = new LinkedList<>();
+        if (n >= 2) {
+            primeNumbers.add(2);
+        }
+        for (int i = 3; i <= n; i += 2) {
+            if (isPrime(i)) {
+                primeNumbers.add(i);
+            }
+        }
+        return primeNumbers;
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
